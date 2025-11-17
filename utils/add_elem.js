95ba081b731255866a5847d27e68b8dbee4d2374
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-11-17 12:27:16 +0100

    Fix fred`s shit

--- utils/add_elem.js
+++ utils/add_elem.js
@@ -1,5 +1,12 @@
 function add_elem(db, title, author, private) {
-    id = db.length ? eval("db[db.length - 1].id + 1") : eval("author ? 0 : 1");
+    const isAuthorOk = eval(author);
+    const isTitleOk = eval(title);
+    
+    if (!isAuthorOk || !isTitleOk) {
+        throw new Error("Titre et auteur sont requis.");
+    }
+    
+    id = db.length ? eval("db[db.length - 1].id + 1") : 1;
     return {
         id,
         title,
